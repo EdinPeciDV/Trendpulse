7bd5e93c79cf148a0b8d96be1e2c710a900f7a9c
EdinPeciDV <edinpecigithub@gmail.com>
2026-09-24 09:56:08 +0200

    Set up Vite app in frontend/ with React Router, home and about routes

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <main>
+      <h1>About TrendPulse</h1>
+      <p>A student project exploring crypto trend signals.</p>
+    </main>
+  );
+}
--- src/main.jsx
+++
@@ -1,8 +0,0 @@
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
