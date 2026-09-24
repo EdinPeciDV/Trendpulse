67d44d6623a0c0bd4853f8b24947e8395426f9f7
EdinPeciDV <edinpeci1@gmail.com>
2026-09-24 09:30:25 +0200

    Add Home component for TrendPulse page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>TrendPulse</h1>
+      <p>Crypto signals, powered by data.</p>
+      {/* signals list, chart, and selector will go here */}
+    </main>
+  );
+}
